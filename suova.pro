56996ff7eb95e315506a2aefa310df1dcc45776d
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2012-06-12 21:27:31 +0300

    Initial commit.
    README file

---
+++ suova.pro
@@ -0,0 +1,19 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2012-06-12T21:19:07
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+TARGET = suova
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        suovawindow.cpp
+
+HEADERS  += suovawindow.h
+
+OTHER_FILES += \
+    README.txt
---
+++ suovawindow.h
@@ -0,0 +1,34 @@
+/**************************************************************************
+**  suovawindow.h
+**  Copyright (c) 2012 Arto Hyvättinen 
+**
+**  This program is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  See <http://www.gnu.org/licenses/>
+**
+**    12.6.2012
+**************************************************************************/
+
+#ifndef SUOVAWINDOW_H
+#define SUOVAWINDOW_H
+
+#include <QtGui/QMainWindow>
+
+class SuovaWindow : public QMainWindow
+{
+    Q_OBJECT
+    
+public:
+    SuovaWindow(QWidget *parent = 0);
+    ~SuovaWindow();
+};
+
+#endif // SUOVAWINDOW_H
